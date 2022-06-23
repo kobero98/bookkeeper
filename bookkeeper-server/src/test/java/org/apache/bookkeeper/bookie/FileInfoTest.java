f3634d9c208a6555959704291264b1f4dfc70bf0
kobero98 <matteo.fede@live.it>
2022-06-23 14:24:35 +0200

    ora JaCoCo esegue correttamente al coverage

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/FileInfoTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/FileInfoTest.java
@@ -1,4 +1,32 @@
 package org.apache.bookkeeper.bookie;
 
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
 public class FileInfoTest {
+
+    @Rule
+    public TemporaryFolder folder=new TemporaryFolder();
+    @Test
+    public void testFileInfoClose()
+    {
+        try {
+            File file= folder.newFile();
+            byte [] masterkey=new byte[]{};
+            FileInfo f= new FileInfo(file,masterkey,0);
+            f.close(true);
+            assertEquals(f.isClosed(),true);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
 }
